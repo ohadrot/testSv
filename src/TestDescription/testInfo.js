30957e3fa7f940a48caa2c567307aaa3d3a7dd6a
ohad rot <ohad@svcollege.co.il>
2023-07-13 15:57:29 +0300

    v1.0

---
+++ src/TestDescription/testInfo.js
@@ -0,0 +1,35 @@
+
+
+export const tests = [{
+    title:'qa',
+    info:'מבחן התאמה לבודקי תוכנה'
+},
+{
+    title:'fullstack',
+    info:'מבחן התאמה למפתחים'
+},
+{
+    title:'cyber',
+    info:'מבחן התאמה לסייבר'
+},
+{
+    title:'gaming',
+    info:'מבחן התאמה לפיתוח משחקים'
+},
+{
+    title:'general',
+    info:'מבחן התאמה כללי'
+},
+{
+    title: 'digitalMarkting',
+    info: 'מבחן התאמה לשיווק דיגיטלי ומדיה חברתית'
+},
+{
+    title: 'fullstack_second_test',
+    info: 'מבחן התאמה למפתחים'
+},
+{
+    title: 'socialMedia',
+    info: 'מבחן התאמה למדיה חברתית'
+}
+]
